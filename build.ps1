fdfddf7805e9d021bf6b38681816021d88cce59a
PeriBluGaming <periblugaming@gmail.com>
2026-08-07 21:30:12 +0200

    last changes

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@
 
 $ErrorActionPreference = 'Stop'
 $Root = Split-Path -Parent $MyInvocation.MyCommand.Path
-cmake -S $Root -B (Join-Path $Root 'build') -G Ninja -DCMAKE_BUILD_TYPE=Debug -DPSX_RECOMP_UI=ON
+cmake -S $Root -B (Join-Path $Root 'build') -G Ninja -DCMAKE_BUILD_TYPE=Debug -DPSX_RECOMP_UI=ON -DPSX_LAUNCHER=ON
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 cmake --build (Join-Path $Root 'build') --config Debug --parallel
 exit $LASTEXITCODE
